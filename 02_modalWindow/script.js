8769caf8f11d6b2150e8f2da078beeeba83c5fa3
jet-web <64885221+jet-web@users.noreply.github.com>
2021-07-30 16:30:08 +0900

    Add files via upload

---
+++ 02_modalWindow/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i += 1)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log('A key was pressed');
+  // console.log(e);
+  console.log(e.key);
+  //^ どのキーが押されたのか、ログされる/書き込まれる。
+  // 閉じるキーは「C (大文字)」にする。
+  if (e.key === 'C') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
